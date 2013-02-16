6d51fdeae7cbba5cd0dc40832ca4af3ca63f5b80
wakeuky <hendren.alex@gmail.com>
2013-02-16 01:43:40 -0500

    New Qt Compile

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sat Feb 16 00:54:04 2013
+** Created: Sat Feb 16 01:42:21 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.4)
 **
 ** WARNING! All changes made in this file will be lost!
--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Sat Feb 16 00:54:01 2013
+** Created: Sat Feb 16 01:42:18 2013
 **      by: Qt User Interface Compiler version 4.8.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
