5cee58971dd608d401c2aef3bab44e167ce17041
wakeuky <hendren.alex@gmail.com>
2013-02-16 01:59:06 -0500

    changed odbc

--- defaults.h
+++ defaults.h
@@ -1,13 +1,13 @@
 #ifndef DEFAULTS_H
 #define DEFAULTS_H
 
-#define SQL_SERVER ""
+#define SQL_SERVER "192.168.1.150"
 #define SQL_PORT 3306
 #define SQL_DATABASE "homelogic"
-#define SQL_USER    ""
-#define SQL_PASS    ""
+#define SQL_USER    "homelogic"
+#define SQL_PASS    "homelogic"
 
-#define SQL_DRIVER "QODBC"
+#define SQL_DRIVER "QODBC3"
 
 /* Define QTimer in ms before interrupt signal */
 #define DEVICE_QUERY_WAIT   2000
